09481fe5f1803a2cb46aa46812945f941f09f549
MichaelYao1998 <yaoyuchen1998@gmial.com>
2019-01-14 16:40:00 +0800

    update weather controller

--- yii framework project/controllers/WeatherController.php
+++ yii framework project/controllers/WeatherController.php
@@ -10,9 +10,12 @@
  * https://www.cnblogs.com/open88/p/7343983.html
  * 数据的调用参考阿里云
  */
+
 namespace app\controllers;
 
 use app\models\Weather;
+use Yii;
+use yii\helpers\Html;
 use yii\web\Controller;
 
 header('Content-type:text/html; charset=utf-8');
@@ -34,21 +37,32 @@ class WeatherController extends Controller
     public function actionForecast()
     {
         $model = new Weather();
-        return $this->render("forecast", ['model' => $model]);
 
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            return $this->render("display", ['model' => $model]);
+        } else {
+            return $this->render('forecast', ['model' => $model]);
+
+        }
     }
-    public function actionDisplay(){
-        return $this->render("display");
+
+    public function actionDisplay()
+    {
+        $model = new Weather();
+        return $this->render("display", ['model' => $model]);
     }
+
     static public function getData()
     {
+        $model = new Weather();
         $host = "https://ali-weather.showapi.com";
         $path = "/area-to-weather";
         $method = "GET";
         $appcode = "371c81cea7784fcfac15d67ccb9dde0a";
         $headers = array();
         array_push($headers, "Authorization:APPCODE " . $appcode);
-        $querys = "area=%E4%B8%BD%E6%B1%9F";
+        $querys = "area=大连";
+//        $querys = "area=" . HTML::encode($model->area);
         $bodys = "";
         $url = $host . $path . "?" . $querys;
 
@@ -59,23 +73,22 @@ class WeatherController extends Controller
         curl_setopt($curl, CURLOPT_FAILONERROR, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HEADER, true);
-        if (1 == strpos("$".$host, "https://"))
-        {
+        if (1 == strpos("$" . $host, "https://")) {
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         }
         curl_setopt($curl, CURLOPT_POSTFIELDS, $bodys);
         $response = curl_exec($curl);
         $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        $headers        = substr($response, 0, $header_size);
-        $body    = substr($response, $header_size);
-        $weather=json_decode($body);
-        echo  "<img src=".$weather->showapi_res_body->f1->day_weather_pic." /><br>";
-        echo  "城市：  ".$weather->showapi_res_body->cityInfo->c5."<br/>";
-        echo  "白天天气：  ".$weather->showapi_res_body->f1->day_weather."<br/>";
-        echo  "晚上天气：  ".$weather->showapi_res_body->f1->night_weather."<br/>";
-        echo  "最低温度：  ".$weather->showapi_res_body->f1->night_air_temperature."<br/>";
-        echo  "最高温度：  ".$weather->showapi_res_body->f1->day_air_temperature."<br/>";
-        echo  "PM2.5：  ".$weather->showapi_res_body->now->aqiDetail->pm2_5." ".$weather->showapi_res_body->now->aqiDetail->quality."<br/>";
+        $headers = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+        $weather = json_decode($body);
+        echo "<img src=" . $weather->showapi_res_body->f1->day_weather_pic . " /><br>";
+        echo "城市：  " . $weather->showapi_res_body->cityInfo->c5 . "<br/>";
+        echo "白天天气：  " . $weather->showapi_res_body->f1->day_weather . "<br/>";
+        echo "晚上天气：  " . $weather->showapi_res_body->f1->night_weather . "<br/>";
+        echo "最低温度：  " . $weather->showapi_res_body->f1->night_air_temperature . "<br/>";
+        echo "最高温度：  " . $weather->showapi_res_body->f1->day_air_temperature . "<br/>";
+        echo "PM2.5：  " . $weather->showapi_res_body->now->aqiDetail->pm2_5 . " " . $weather->showapi_res_body->now->aqiDetail->quality . "<br/>";
     }
 }
