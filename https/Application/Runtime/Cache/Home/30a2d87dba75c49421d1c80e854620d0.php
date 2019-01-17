0f06b8e82ee61080f74f2544f65c72cbab7051a9
MichaelYao1998 <yaoyuchen1998@gmial.com>
2019-01-17 10:52:47 +0800

    update cache

--- https/Application/Runtime/Cache/Home/30a2d87dba75c49421d1c80e854620d0.php
+++ https/Application/Runtime/Cache/Home/30a2d87dba75c49421d1c80e854620d0.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <?php
- function curl_get_https($url) { $curl = curl_init(); curl_setopt($curl,CURLOPT_URL,$url); curl_setopt($curl,CURLOPT_HEADER,false); curl_setopt($curl,CURLOPT_RETURNTRANSFER,false); curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false); curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,2); $tempInfo = curl_exec($curl); curl_close($curl); return $tempInfo; } $url = "https://www.baidu.com"; $result = curl_get_https($url); var_dump($result); ?>
+ function curl_get_https($url) { $curl = curl_init(); curl_setopt($curl,CURLOPT_URL,$url); curl_setopt($curl,CURLOPT_HEADER,false); curl_setopt($curl,CURLOPT_RETURNTRANSFER,false); curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false); curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,2); $tempInfo = curl_exec($curl); curl_close($curl); return $tempInfo; } $url = "https://www.taobao.com"; $result = curl_get_https($url); var_dump($result); ?>
 
 </body>
 </html>
